1d08264955dee3a14f8efdab2be22048c5a8bf52
Eli Cusic <tntitans31@gmail.com>
2020-12-01 09:32:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +8,60 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function createEmployeeRecord(arr){
+    let obj = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj
+}
+
+function createEmployeeRecords(arr){
+    return arr.map(x => createEmployeeRecord(x))
+}
+
+function createTimeInEvent(str){
+    let dateArr = str.split(" ") 
+    let newEvent = {
+        type: "TimeIn",
+        hour: parseInt(dateArr[1], 10),
+        date: dateArr[0]
+    }
+    this.timeInEvents.push(newEvent)
+    return this
+}
+
+function createTimeOutEvent(str){
+    let arr = str.split(" ")
+    let event = {
+        type: "TimeOut",
+        hour: parseInt(arr[1], 10),
+        date: arr[0]
+    }
+    this.timeOutEvents.push(event)
+    return this
+}
+
+function hoursWorkedOnDate(str){
+    let ins = this.timeInEvents.find(x => x.date === str)
+    let out = this.timeOutEvents.find(x => x.date === str)
+    return (out.hour/100) - (ins.hour/100)
+}
+
+function wagesEarnedOnDate(str){
+    return this.payPerHour * hoursWorkedOnDate.call(this, str)
+}
+
+function calculatePayroll(arr){
+    return arr.map(x => allWagesFor.call(x)).reduce((x,y) => x += y)
+}
+
+function findEmployeeByFirstName(collection, firstNameString){
+    return collection.find(x => x.firstName === firstNameString)
 }
